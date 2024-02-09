3e864079ab3505f1d34396a16b8199061d911eb1
tfermoso <tomas.fermoso@gmail.com>
2024-02-09 17:29:35 +0100

    Método Login

--- src/main/java/com/ceica/taskappfx/controllerviews/LoginController.java
+++ src/main/java/com/ceica/taskappfx/controllerviews/LoginController.java
@@ -1,4 +1,27 @@
 package com.ceica.taskappfx.controllerviews;
 
+import com.ceica.taskappfx.controller.TaskController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    @FXML
+    protected TextField txtUsername;
+    @FXML
+    protected PasswordField txtPassword;
+    @FXML
+    protected Label lblMessage;
+
+    public TaskController taskController=new TaskController();
+
+    public void btnLogin(ActionEvent actionEvent) {
+        if(taskController.login(txtUsername.getText(),txtPassword.getText())){
+            System.out.println("TODO OK");
+        }else {
+            lblMessage.setText("Incorrect User or Password");
+        }
+    }
 }
