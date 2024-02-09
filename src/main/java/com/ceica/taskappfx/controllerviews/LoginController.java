9c6b502568f07cab00ee62966b9527aec6042967
tfermoso <tomas.fermoso@gmail.com>
2024-02-09 19:23:21 +0100

    Mostrando ventana User o Admin

--- src/main/java/com/ceica/taskappfx/controllerviews/LoginController.java
+++ src/main/java/com/ceica/taskappfx/controllerviews/LoginController.java
@@ -1,11 +1,19 @@
 package com.ceica.taskappfx.controllerviews;
 
+import com.ceica.taskappfx.TaskApplication;
 import com.ceica.taskappfx.controller.TaskController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
     @FXML
@@ -19,12 +27,33 @@ public class LoginController {
 
     public void btnLogin(ActionEvent actionEvent) {
         if(taskController.login(txtUsername.getText(),txtPassword.getText())){
+            String view="";
+            String titleWindow;
             if(taskController.isAdmin()){
                 //Mostrar vista administrador
-                System.out.println("admin");
+                view="admin-view.fxml";
+                titleWindow="Admin";
             }else{
                 //Mostrar vista usuario
-                System.out.println("user");
+                view="user-view.fxml";
+                titleWindow="User";
+            }
+            FXMLLoader fxmlLoader=new FXMLLoader(TaskApplication.class.getResource(view));
+            try {
+                Parent root=fxmlLoader.load();
+                //UserController userController =loader.getController();
+                Scene scene=new Scene(root);
+                Stage stage=new Stage();
+                stage.setTitle(titleWindow);
+                stage.setScene(scene);
+                stage.show();
+                Node source=(Node) actionEvent.getSource();
+                Stage stage1=(Stage) source.getScene().getWindow();
+                stage1.close();
+
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }else {
             lblMessage.setText("Incorrect User or Password");
